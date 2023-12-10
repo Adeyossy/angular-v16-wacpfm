2adf805c4ebdfcf93b87268e741d74461d3626f1
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-10 18:52:33 +0100

    feat: added auth service, home, register, login and verify-email components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent, title:"Faculty of Family Medicine App, West African College of Physicians" },
+  { path: "access/register", component: RegisterComponent },
+  { path: "access/register/verifyemail", component: RegisterComponent },
+  { path: "access/login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
