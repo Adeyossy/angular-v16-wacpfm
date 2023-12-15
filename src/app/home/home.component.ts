1e7f777487603ab95f658e5e208f38f850adaeff
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-15 19:34:25 +0100

    feat: working on verify-email component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  user$ = new Observable<User | null>();
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getFirebaseUser$();
+  }
 }
