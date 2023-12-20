3fd6df3facd18e2a631dce2d453da6112777c2e7
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-20 04:42:17 +0100

    feat: worked on select component widget and placed it in profile registration component

--- src/app/widgets/select/select.component.ts
+++ src/app/widgets/select/select.component.ts
@@ -1,22 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit {
   @Input() mode = "";
   @Input() items: string[] = [];
-  @Input() itemsSelectionState: boolean[] = [];
+  itemsSelectionState: boolean[] = [];
   @Input() selectedItems: string[] = [];
   @Output() emitItems = new EventEmitter<string[]>();
 
+  ngOnInit(): void {
+    this.itemsSelectionState = this.items.map(_item => false);
+  }
+
   toggleItem(index: number) {
+    console.log('index => ', index);
+    console.log('itemsSelectionState => ', this.itemsSelectionState[index]);
     if (this.mode === "single") {
       const itemState = this.itemsSelectionState[index];
       this.itemsSelectionState = this.itemsSelectionState.fill(false);
-      this.itemsSelectionState[index] = itemState;
+      this.itemsSelectionState[index] = !itemState;
     } else {
       this.itemsSelectionState[index] = !this.itemsSelectionState[index];
     }
