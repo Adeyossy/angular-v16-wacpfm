5c2f314c9ba0337ce93896253a1736a05f0bd91f
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-12-30 12:26:13 +0100

    feat: added a vector image for showing empty item

--- src/app/update-course/update-course.component.ts
+++ src/app/update-course/update-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-update-course',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-course.component.css']
 })
 export class UpdateCourseComponent {
+  doc = null;
 
+  constructor(private authService: AuthService) {
+  }
 }
