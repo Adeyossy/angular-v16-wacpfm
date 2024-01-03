4a869aedbf23e861775b8bd57f8a29446f54ef56
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-03 14:25:07 +0100

    feat: add getTodaysDate method to helper.service.ts

--- src/app/services/helper.service.ts
+++ src/app/services/helper.service.ts
@@ -14,4 +14,17 @@ export class HelperService {
   millisToDate(millis: number) {
     return new Date(millis);
   }
+
+  getDateString(millis: number) {
+    const date = new Date(millis);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDay();
+    // return `${year}-${month}-${day}`;
+    return date.toISOString().substring(0, 10);
+  }
+
+  getTodaysDate() {
+    return this.getDateString(Date.now());
+  }
 }
