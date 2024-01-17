1c27400da71a280bb0abfac1ac26c9dc8fda5b82
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-17 17:58:57 +0100

    feat: Include payment options and accenting

--- src/app/update-course/update-course-payment/update-course-payment.component.ts
+++ src/app/update-course/update-course-payment/update-course-payment.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-update-course-payment',
   templateUrl: './update-course-payment.component.html',
   styleUrls: ['./update-course-payment.component.css']
 })
-export class UpdateCoursePaymentComponent {
+export class UpdateCoursePaymentComponent implements OnInit, OnDestroy {
+  routeSub = new Subscription();
+  
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.paramMap.subscribe({
+      next: params => {}
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
+
+  uploadReceipt() {
+    // Use transactions for multiple uploads
+    // When payment receipt upload is done
+  }
 }
