29bbda1a6238305d52678b73586ee08fe2428962
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-24 08:04:59 +0100

    feat: Configure profile guard

--- src/app/profile.guard.ts
+++ src/app/profile.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { concatMap, map } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { USERS } from './models/user';
 
 export const profileGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  return authService.getDocByUserId$(USERS).pipe(
+    map(doc => doc.exists() ? true : router.parseUrl('/profile/registration'))
+  )
 };
