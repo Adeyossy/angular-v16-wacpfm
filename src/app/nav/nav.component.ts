778452096e1dedb228a04a920b621e40faff7018
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-24 13:02:00 +0100

    feat: Navigate back home on sign out

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from 'firebase/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,13 +12,14 @@ import { User } from 'firebase/auth';
 export class NavComponent {
   user$ = new Observable<User | null>();
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.user$ = authService.getFirebaseUser$();
   }
 
   signOut() {
     const subscription = this.authService.signOut$().subscribe({
       next: (_value) => {
+        this.router.navigateByUrl("/");
         subscription.unsubscribe();
       },
       error: err => console.log("Error occurred while logging out => ", err)
