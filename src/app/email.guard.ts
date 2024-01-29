f2fecce3134c8f573294caab321a93230dcf72d5
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-29 13:53:21 +0100

    feat: Configure email verification guard

--- src/app/email.guard.ts
+++ src/app/email.guard.ts
@@ -1,5 +1,12 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 export const emailGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  return authService.getFirebaseUser$().pipe(
+    map(user => user?.emailVerified ? true : router.parseUrl('/access/register/verifyemail'))
+  );
 };
