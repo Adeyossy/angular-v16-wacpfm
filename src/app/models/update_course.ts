7f98688c691ffd04ba3f3a5856f2f14e8a6fb193
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-01-31 17:29:50 +0100

    feat: Add revised update course model

--- src/app/models/update_course.ts
+++ src/app/models/update_course.ts
@@ -1,6 +1,14 @@
 export const UPDATE_COURSES = "update_courses";
 export const UPDATE_COURSES_LECTURES = "update_course_lectures";
 
+export type UpdateCourseDetails = {
+  theme: string;
+  participants: string[];
+  lectures: string[];
+  certificate: string;
+  releaseResources: boolean;
+}
+
 export type UpdateCourse = {
   updateCourseId: string;
   title: string;
@@ -21,6 +29,21 @@ export type UpdateCourse = {
   totLectures: string[];
   totTheme: string;
   totParticipants: string[];
+  totUpdateParticipants: string[];
+  resourcePersons: string[];
+}
+
+export type UpdateCourseRev = {
+  updateCourseId: string;
+  title: string;
+  creator: string;
+  registrationOpenDate: number;
+  registrationCloseDate: number;
+  startDate: number;
+  endDate: number;
+  membership: UpdateCourseDetails;
+  fellowship: UpdateCourseDetails;
+  tot: UpdateCourseDetails;
   totUpdateParticipants: string[];
   resourcePersons: string[];
 }
