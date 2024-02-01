912f4feb74923679c8804c032b46401186088266
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-01 09:34:20 +0100

    feat: Add id to update course record model

--- src/app/models/update_course_record.ts
+++ src/app/models/update_course_record.ts
@@ -3,6 +3,7 @@ import { Payment } from "./payment";
 export const UPDATE_COURSES_RECORDS = "update_course_records";
 
 export type UpdateCourseRecord = {
+  id: string;
   updateCourseId: string;
   userId: string;
   userEmail: string;
