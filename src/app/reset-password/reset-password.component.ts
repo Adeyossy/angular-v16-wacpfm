37bd3bc9add6ccde962ec9f61557e1a65d1fff1c
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-02-07 12:19:18 +0100

    feat: Add logic to password reset component

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { EMPTY, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+  email = "";
+  reset$ = new Observable<void>();
+
+  constructor(private authService: AuthService) {}
+
+  resetPassword() {
+    this.reset$ = this.authService.resetPassword$(this.email);
+  }
+
+  dismissOverlay() {
+    this.reset$ = EMPTY;
+  }
 
 }
