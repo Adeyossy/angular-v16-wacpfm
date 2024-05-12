72426dd761ff771e1a3a57ecd447641de5509490
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-05-12 06:04:38 +0100

    feat: Add UpdateCourseType for update course records

--- src/app/models/update_course_record.ts
+++ src/app/models/update_course_record.ts
@@ -2,12 +2,14 @@ import { Payment } from "./payment";
 
 export const UPDATE_COURSES_RECORDS = "update_course_records";
 
+export type UpdateCourseType = "Membership" | "Fellowship" | "ToT";
+
 export type UpdateCourseRecord = {
   id: string;
   updateCourseId: string;
   userId: string;
   userEmail: string;
-  courseType: "Membership" | "Fellowship" | "ToT";
-  paymentId: Payment;
+  courseType: UpdateCourseType;
+  paymentId: Payment | null;
   paymentEvidence?: string;
 }
