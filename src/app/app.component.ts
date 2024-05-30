091d705582ded9ee070b84a62a556f513c10c3b5
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-05-30 01:27:49 +0100

    feat: Use central overlay for dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HelperService } from './services/helper.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wacpfm';
+  constructor(public helper: HelperService) {}
 }
