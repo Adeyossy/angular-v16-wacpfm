14820fdc6cc895f3ae678090cacb081918881d73
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-06-07 08:13:44 +0100

    feat: Extend OverlayComponent

--- src/app/component-overlay/component-overlay.component.ts
+++ src/app/component-overlay/component-overlay.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OverlayComponent } from '../overlay/overlay.component';
 
 @Component({
   selector: 'app-component-overlay',
   templateUrl: './component-overlay.component.html',
-  styleUrls: ['./component-overlay.component.css']
+  styleUrls: ['./component-overlay.component.css', "../overlay/overlay.component.css"]
 })
-export class ComponentOverlayComponent {
-
+export class ComponentOverlayComponent extends OverlayComponent {
 }
