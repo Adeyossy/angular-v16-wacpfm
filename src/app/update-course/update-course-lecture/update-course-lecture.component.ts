b2d6a920cbdd4ba346fb12595649fd250c6c4d72
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-06-07 18:55:33 +0100

    feat: Configure update course lecture component

--- src/app/update-course/update-course-lecture/update-course-lecture.component.ts
+++ src/app/update-course/update-course-lecture/update-course-lecture.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UpdateCourseLecture } from 'src/app/models/update_course';
+import { UPDATE_COURSE_TYPES, UpdateCourseType } from 'src/app/models/update_course_record';
 
 @Component({
   selector: 'app-update-course-lecture',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-course-lecture.component.css']
 })
 export class UpdateCourseLectureComponent {
+  @Input() updateCourseId = "";
+  @Input() lecture: UpdateCourseLecture = {
+    courseType: "Membership",
+    lectureId: "",
+    lecturerId: "",
+    lecturerEmail: "",
+    lectureTitle: "",
+    updateCourseId: this.updateCourseId,
+    startTime: Date.now().toString(),
+    endTime: String(Date.now() + (60 * 60)),
+    materialLink: [],
+    videoLink: ""
+  }
+  courseTypes = UPDATE_COURSE_TYPES.slice();
 
+  setCourseType(value: string[]) {
+    if(value.length) {
+      this.lecture.courseType = value[0] as UpdateCourseType
+    } 
+  }
 }
