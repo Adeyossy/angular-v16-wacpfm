83477d971b4c6ca1935a5206fdb040a5301e9c27
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-06-19 21:14:40 +0100

    fix: Show both tot and resident course participants

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { NEVER, Observable, concatMap, map, timer } from 'rxjs';
 import { UPDATE_COURSES_RECORDS, UPDATE_COURSE_TYPES, UpdateCourseRecord, UpdateCourseType } from '../models/update_course_record';
@@ -11,16 +11,20 @@ import { HelperService } from '../services/helper.service';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, DoCheck {
   courses$: Observable<UpdateCourse[]> = NEVER;
   records$: Observable<UpdateCourseRecord[]> = NEVER;
-  list$: Observable<CardList[]> = new Observable();
+  // list$: Observable<CardList[]> = new Observable();
   level = 0;
   sublevel = 0;
   currentCourse: UpdateCourse | null = null;
   courseTypes = UPDATE_COURSE_TYPES;
   newLecture$: Observable<UpdateCourseLecture> = NEVER;
   lectures$: Observable<UpdateCourseLecture[]> = NEVER;
+  grantAccess$: Observable<void> = NEVER;
+
+  // should lecture is a flag to let the system knoww that
+  shouldRefresh: "lecture" | "payment" | "resource_person" | "" = "";
 
   constructor(private authService: AuthService, private helper: HelperService) { }
 
@@ -51,6 +55,29 @@ export class AdminComponent implements OnInit {
     this.createNewLecture();
   }
 
+  ngDoCheck(): void {
+    if (this.helper.isDialogShown === -1 && this.shouldRefresh) {
+      switch (this.shouldRefresh) {
+        case "lecture":
+          this.setToLectures();
+          break;
+
+        case "payment":
+          this.setToPayment();
+          break;
+
+        case "resource_person":
+          this.setToResourcePersons();
+          break;
+
+        default:
+          break;
+      }
+
+      this.shouldRefresh = "";
+    }
+  }
+
   courseToCardList(course: UpdateCourse) {
     return {
       title: course.title,
@@ -65,8 +92,9 @@ export class AdminComponent implements OnInit {
   }
 
   hybridize(records: UpdateCourseRecord[]) {
+    const newRecords: UpdateCourseRecord[] = [];
     for (let i = 0; i < records.length; i++) {
-      let currentRecord = records[i];
+      let currentRecord = Object.assign({}, records[i]);
       for (let j = i + 1; j < records.length; j++) {
         let nextRecord = records[j];
         if (currentRecord.userEmail === nextRecord.userEmail) {
@@ -75,8 +103,11 @@ export class AdminComponent implements OnInit {
           }
         }
       }
+      if (newRecords.find(n => n.userEmail === records[i].userEmail) === undefined) {
+        newRecords.push(Object.assign({}, currentRecord))
+      }
     }
-    return records;
+    return newRecords;
   }
 
   setToPayment() {
@@ -86,30 +117,29 @@ export class AdminComponent implements OnInit {
       "updateCourseId", "==", this.currentCourse?.updateCourseId!).pipe(
         map(snapshot => snapshot.docs.map(doc => {
           const record = doc.data() as UpdateCourseRecord;
-          if(record.id !== doc.id) record.id = doc.id;
+          if (record.id !== doc.id) record.id = doc.id;
           return record;
-        })),
-        map(this.hybridize)
+        }))
       );
 
-    this.list$ = this.records$.pipe(
-      map(records => {
-        const newRecs: CardList[] = [];
-        for (let i = 0; i < records.length; i++) {
-          let rec = records[i];
-          let existingIndex = newRecs.findIndex(c => c.title === rec.userEmail);
-          if (existingIndex >= 0) {
-            newRecs[existingIndex].subtitle += `, ${rec.courseType}`;
-            newRecs[existingIndex].subtitle = newRecs[existingIndex].subtitle.split(", ")
-              .sort().join(", ");
-          } else {
-            if (rec.userEmail === "adeyossy1@gmail.com") continue;
-            newRecs.push({ title: rec.userEmail, subtitle: rec.courseType, text: "" })
-          }
-        }
-        return newRecs;
-      })
-    );
+    // this.list$ = this.records$.pipe(
+    //   map(records => {
+    //     const newRecs: CardList[] = [];
+    //     for (let i = 0; i < records.length; i++) {
+    //       let rec = records[i];
+    //       let existingIndex = newRecs.findIndex(c => c.title === rec.userEmail);
+    //       if (existingIndex >= 0) {
+    //         newRecs[existingIndex].subtitle += `, ${rec.courseType}`;
+    //         newRecs[existingIndex].subtitle = newRecs[existingIndex].subtitle.split(", ")
+    //           .sort().join(", ");
+    //       } else {
+    //         if (rec.userEmail === "adeyossy1@gmail.com") continue;
+    //         newRecs.push({ title: rec.userEmail, subtitle: rec.courseType, text: "" })
+    //       }
+    //     }
+    //     return newRecs;
+    //   })
+    // );
   }
 
   setToLectures() {
@@ -119,7 +149,7 @@ export class AdminComponent implements OnInit {
       "updateCourseId", "==", this.currentCourse!.updateCourseId).pipe(
         map(snapshot => snapshot.docs.map(doc => {
           const lecture = doc.data() as UpdateCourseLecture;
-          if(lecture.lectureId !== doc.id) lecture.lectureId = doc.id;
+          if (lecture.lectureId !== doc.id) lecture.lectureId = doc.id;
           return lecture;
         }))
       );
@@ -157,20 +187,28 @@ export class AdminComponent implements OnInit {
     }
   }
 
-  courseTypeOnly<Type extends {courseType: UpdateCourseType}>(records: Type[], courseType: UpdateCourseType) {
+  courseTypeOnly<Type extends { courseType: UpdateCourseType }>(records: Type[], courseType: UpdateCourseType) {
     return records.filter(record => record.courseType === courseType)
   }
 
+  getHybridParticipants(records: UpdateCourseRecord[]) {
+    const hybrids = this.hybridize(records)
+    return hybrids.filter(record => record.courseType.split(", ").sort().join(", ")
+      === "Fellowship, Membership, ToT");
+  }
+
   showLecture(lecture: UpdateCourseLecture) {
-    this.helper.setComponentDialogData({ 
+    this.helper.setComponentDialogData({
       courseId: this.currentCourse!.updateCourseId,
       lecture,
       payment: this.helper.data.payment,
       course: this.helper.data.course
     });
-    
+
     this.helper.toggleDialog(1);
     this.createNewLecture();
+
+    this.shouldRefresh = "lecture";
   }
 
   showPayment(record: UpdateCourseRecord) {
@@ -182,5 +220,37 @@ export class AdminComponent implements OnInit {
     });
 
     this.helper.toggleDialog(1);
+
+    this.shouldRefresh = "payment";
+  }
+
+  byCourseType(courseType: UpdateCourseType, emails: string) {
+    if (courseType === "Membership") {
+      return { membershipParticipants: emails }
+    }
+
+    if (courseType === "Fellowship") {
+      return { fellowshipParticipants: emails }
+    }
+
+    if (courseType === "ToT") {
+      return { totParticants: emails }
+    }
+
+    return {}
+  }
+
+  grantAllAccess(courseType: UpdateCourseType) {
+    this.grantAccess$ = this.authService.getDoc$<UpdateCourse>(UPDATE_COURSES,
+      this.currentCourse!.updateCourseId).pipe(
+        concatMap(course => {
+          return this.records$.pipe(
+            map(recs => this.courseTypeOnly(recs, courseType).filter(rec => rec.approved === true)
+              .map(rec => rec.userEmail).join(", ")),
+            concatMap(emails => this.authService.updateDoc$(UPDATE_COURSES, course.updateCourseId,
+              this.byCourseType(courseType, emails)))
+          )
+        })
+      )
   }
 }
