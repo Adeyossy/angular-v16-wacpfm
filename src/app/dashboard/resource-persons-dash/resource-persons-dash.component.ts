6faaf45283c431780ac01818783bbe389890c1a2
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-06-30 06:02:10 +0100

    feat: Show resource person component per course

--- src/app/dashboard/resource-persons-dash/resource-persons-dash.component.ts
+++ src/app/dashboard/resource-persons-dash/resource-persons-dash.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, concatMap, iif, map, of } from 'rxjs';
+import { RESOURCE_PERSONS, ResourcePerson } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-resource-persons-dash',
   templateUrl: './resource-persons-dash.component.html',
   styleUrls: ['./resource-persons-dash.component.css']
 })
-export class ResourcePersonsDashComponent {
+export class ResourcePersonsDashComponent implements OnInit {
+  resourcePersons$: Observable<ResourcePerson[]> = new Observable();
 
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.resourcePersons$ = this.activatedRoute.paramMap.pipe(
+      concatMap(p => {
+        const id = p.get("updateCourseId");
+        return iif(
+          () => id !== "", 
+          this.resourcePersons$.pipe(map(ls => ls.filter(l => l.updateCourseId === id))),
+          of([])
+        )
+      })
+    );
+  }
 }
