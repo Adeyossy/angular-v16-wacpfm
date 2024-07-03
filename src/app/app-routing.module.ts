628f948ff6e11f796190ded7cda7ebb774dabd67
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-07-03 07:43:55 +0100

    feat: Fetch resource persons via email from route param

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,7 +40,7 @@ const routes: Routes = [
       { path: "updatecourse/:updateCourseId/details", component: UpdateCourseDetailsComponent },
       { path: "updatecourse/:updateCourseId/details/payment/:category", component: UpdateCoursePaymentComponent },
       { path: "updatecourse/:updateCourseId/details/edit", component: NewCourseComponent },
-      { path: "updatecourse/:updateCourseId/resourcepersons", component: ResourcePersonsDashComponent },
+      { path: "updatecourse/:updateCourseId/resourcepersons/:email", component: ResourcePersonsDashComponent },
       { path: "updatecourse/:updateCourseId/details/certificate/:recordId", component: CertificateComponent },
       { path: "admin", component: AdminComponent, canActivate: [adminGuard] }
     ]
--- src/app/dashboard/resource-persons-dash/resource-persons-dash.component.ts
+++ src/app/dashboard/resource-persons-dash/resource-persons-dash.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { where } from 'firebase/firestore';
 import { Observable, concatMap, iif, map, of } from 'rxjs';
 import { RESOURCE_PERSONS, ResourcePerson } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
@@ -18,10 +19,13 @@ export class ResourcePersonsDashComponent implements OnInit {
     this.resourcePersons$ = this.activatedRoute.paramMap.pipe(
       concatMap(p => {
         const id = p.get("updateCourseId");
+        const email = p.get("email");
         return iif(
-          () => id !== "", 
-          this.authService.queryByUserEmail$<ResourcePerson>(RESOURCE_PERSONS)
-            .pipe(map(ls => ls.filter(l => l.updateCourseId === id))),
+          () => id !== "" && email !== "", 
+          this.authService.queriesCollections$<ResourcePerson>(RESOURCE_PERSONS, [
+            where("updateCourseId", "==", id),
+            where("userEmail", "==", email)
+          ]),
           of([])
         )
       })
