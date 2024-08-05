9abb1994d3b0271da9c554bedb0c85a0c4eb5d0f
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-08-05 10:52:32 +0100

    fix: Flatten object into individual properties as parameters

--- src/app/widgets/input/input.component.ts
+++ src/app/widgets/input/input.component.ts
@@ -1,20 +1,29 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+type InputType = {
+  type: string;
+  name: string;
+  label: string;
+  placeholder: string;
+  value: string | number;
+  required: boolean;
+  disabled: boolean;
+}
+
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-  @Input() attributes = {
-    type: "text",
-    name: "",
-    label: "",
-    placeholder: "",
-    value: "",
-    required: true,
-    disabled: false,
-    icon: "circle-fill"
-  }
+  @Input() type = "text";
+  @Input() name = "";
+  @Input() label = "";
+  @Input() placeholder = "";
+  @Input() value: string | number = "";
+  @Input() required = true;
+  @Input() disabled = true;
+  @Input() icon = "circle-fill";
+  
   @Output() emitValue = new EventEmitter<string>();
 }
