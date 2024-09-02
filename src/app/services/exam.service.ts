d5aa17dee04eb0c3fb9d6d65999e2834e357f34e
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-09-02 17:39:33 +0100

    feat: Add AuthService dependency

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExamService {
+  // exam registration
+  // exam information
+  // exam progress
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 }
