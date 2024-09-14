4c8e65117eae7898e435d5a00337e57d06cbfaae
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-09-14 11:06:14 +0100

    feat: Create examination environment variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,11 @@ export const environment = {
   updateCourse: "test_update_courses",
   updateCourseRecord: "test_update_course_records",
   resourcePersons: "test_resource_persons",
-  updateCourseLecture: "test_update_course_lectures"
+  updateCourseLecture: "test_update_course_lectures",
+  examiner: "test_examiners",
+  membershipExam: "test_membership_exams",
+  fellowshipExam: "test_fellowship_exams",
+  membershipCandidate: "test_membership_candidates",
+  fellowshipCandidate: "test_fellowship_candidates",
+  referee: "test_referees"
 };
