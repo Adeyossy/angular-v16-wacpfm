07203406a0e54b39409cffca7f11fb7992bef9f4
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-09-14 17:16:01 +0100

    feat: Fetch exam items with local caching

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import { EXAMINERS, Examiner, FellowshipExamRecord, MembershipExamRecord } from '../models/exam';
+import { map, of } from 'rxjs';
+
+type Cache = {
+  examiner: Examiner | null;
+  membership_candidate: MembershipExamRecord | null;
+  fellowship_candidate: FellowshipExamRecord | null;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +16,40 @@ export class ExamService {
   // exam registration
   // exam information
   // exam progress
+  examinerCache: Examiner | null = null;
+  cache: Cache = {
+    examiner: null,
+    membership_candidate: null,
+    fellowship_candidate: null
+  }
 
   constructor(private authService: AuthService) {}
+
+  getExamItem$<Type>(collectionName: string, cacheKey: "examiner" | "membership_candidate" 
+    | "fellowship_candidate") {
+    let cachedItem = this.cache[cacheKey];
+    if (cachedItem !== null) return of(cachedItem);
+    return this.authService.getDocByUserId$<Type>(collectionName).pipe(
+      map(examItem => {
+        switch (cacheKey) {
+          case "examiner":
+            cachedItem = examItem as Examiner;
+            return cachedItem;
+
+          case "membership_candidate":
+            cachedItem = examItem as MembershipExamRecord;
+            return cachedItem;
+
+          case "fellowship_candidate":
+            cachedItem = examItem as FellowshipExamRecord;
+            return cachedItem;
+          
+          default:
+            return null;
+        }
+      })
+    );
+  }
+
+  getFellowshipCandidate$() {}
 }
