996f6b1622cedc5a1f7c13e5cadffadab01c3a14
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-09-27 20:02:49 +0100

    feat: Configure examiner registration

--- src/app/exam/examiner-registration/examiner-registration.component.ts
+++ src/app/exam/examiner-registration/examiner-registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Examiner, EXAMINERS } from 'src/app/models/exam';
+import { Examiner, EXAMINERS, Geopolitical } from 'src/app/models/exam';
 import { AppUser } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { ExamService } from 'src/app/services/exam.service';
@@ -20,6 +20,7 @@ export class ExaminerRegistrationComponent implements OnInit {
     country: "",
     geopolitical: "",
     dateOfBirth: "",
+    nameOfInstitution: "",
     wacpMembershipStatus: "",
     residentDoctorsNo: 0,
     trainingCentre: "",
@@ -39,16 +40,25 @@ export class ExaminerRegistrationComponent implements OnInit {
     specifyMgtExperience: "",
     trainingResponsibilities: "",
     residentsMentored: 0,
-    referees: []
+    referees: {
+      candidateId: "",
+      email: "",
+      id: "",
+      institution: "",
+      name: "",
+      phoneNumber: 0,
+      reasonIfIncorrect: "",
+      response: ""
+    }
   }
 
   examiner$: Observable<Examiner> = new Observable();
   appUser$: Observable<AppUser> = new Observable();
 
-  geopolitical = ["North Central", "North East", "North West", "South West", "South East", 
+  geopolitical = ["North Central", "North East", "North West", "South West", "South East",
     "South South"] as const;
 
-  wacpMembershipStatus = ["Life member", "Paid-up currently", "Paid-up last year", 
+  wacpMembershipStatus = ["Life member", "Paid-up currently", "Paid-up last year",
     "Paid-up 2 years ago"] as const;
 
   currentEmploymentStatus = ["Employed", "Retired"] as const;
@@ -57,16 +67,47 @@ export class ExaminerRegistrationComponent implements OnInit {
 
   doctorsEducatorsTrainingStatus = ["Done", "Not done", "Equivalent"];
 
-  trainingResponsibilities = ["IRTC", "CMEC", "Mentor"];
+  trainingResponsibilities = ["Institutional Residency TC", "CME Coordinator", "Mentor to Residents"];
 
-  constructor(private authService: AuthService, private examService: ExamService) {}
+  accreditation = [
+    "44 NAR, Hosp., Kaduna", "ABUTH, Zaria", "AKTH, Kano", "Askoro District Hospital, FCT",
+    "ATBUTH, Bauchi", "BMC, Saki", "BUTH(ECWA Hospital), Jos", "BUTH (Baptist MC) Ogbomoso",
+    "BMSH, Port Harcourt", "BSUTH, Makurdi", "OLA Catholic Hosp, Oluyuro,Ibadan",
+    "Central Hospital, Benin City", "DASH, Lafia,  Jos", "ECWA Hospital, Egbe,Kogi State",
+    "EKO Hospital, Ikeja, Lagos", "Eku Baptist Hospital, Eku", "EKSUTH, Ado-Ekiti",
+    "Faith Mediplex Hosp, Edo State", "FMC, Keffi, Nassarawa State", "Fed Teach Hosp, Abakaliki",
+    "FMC, Makurdi", "FMC, Abeokuta", "FMC, Asaba", "FMC, Bida, Niger State", "FMC, Ebute-Metta",
+    "FMC, Lokoja", "FMC, Owerri", "Fed Teaching Hosp, Ido-Ekiti", "FMC, Owo", "FMC,( QEH),Umuahia",
+    "Garki  Hospital, Abuja", "General Hospital, Marina, Lagos", "IMSUTH, Imo State",
+    "ISTH, Irrua, Edo State", "Jericho Specialist Hosp, Oyo", "JUTH, Jos", "KSSH, Lokoja",
+    "LASUTH, Ikeja, Lagos", "LAUTECH TH, Osogbo", "LAUTECH TH, Ogbomoso", "LUTH, Lagos",
+    "Maitama District Hopsital, Abuja", "MMS Hosp., Kano", "NAUTH, Nnewi", "NHA, Abuja, FCT",
+    "OAUTHC, Ile-Ife", "OOUTH, Sagamu", "OLA Catholic Hospital, Jos", "PSSH, Jos",
+    "Sacred Heart  Hospital, Lantoro", "SDA, Hosp, Ile-Ife", "St. Gerard's Cath Hosp, Kaduna",
+    "St. Mary's Catholic Hosp. Eleta, Ibadan", "St. Nicholas Hospital, Lagos",
+    "State Specialist Hosp, Sekonu", "UATH, FCT, Abuja", "UBTH, Benin City",
+    "UCH, Ibadan", "UCTH, Calabar", "UDUTH, Sokoto", "UITH, Ilorin",
+    "UNILAG Medical Centre, Lagos", "UNTH (Ituku-Ozalla) Enugu", "UPTH, Port Harcourt",
+    "UUTH, Uyo", "Blue Cross Hospital, Sierra Leone", "Liberia"
+  ];
+
+  constructor(private authService: AuthService, private examService: ExamService) { }
 
   ngOnInit(): void {
     this.appUser$ = this.authService.getAppUser$();
   }
 
   getExaminer$() {
-    this.examiner$ = this.examService.getExamItem$<Examiner>(EXAMINERS, 
+    this.examiner$ = this.examService.getExamItem$<Examiner>(EXAMINERS,
       "examiner") as Observable<Examiner>;
+  }
+
+  geopoliticise(zones: string[]) {
+    return zones[0] as Geopolitical;
+  }
+
+  toBoolean(dichot: string[]) {
+    if (dichot.includes("Yes")) return true;
+    return false;
   }
 }
