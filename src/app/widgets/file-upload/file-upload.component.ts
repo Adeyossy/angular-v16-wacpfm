0ce610de06d8fb82de9e42914bf0549303b707c5
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-10-07 05:23:11 +0100

    feat: Select file to upload one at a time

--- src/app/widgets/file-upload/file-upload.component.ts
+++ src/app/widgets/file-upload/file-upload.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+interface FilePlus extends File {
+  blobURL: string;
+  cloudURL: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-file-upload',
@@ -6,5 +12,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent {
+  @Input() formats: string = "";
+  @ViewChild('file') uploadFile: ElementRef = new ElementRef('input');
+  files: FilePlus[] = [];
 
+  onFileChosen() {
+    const fileElement = this.uploadFile.nativeElement as HTMLInputElement;
+    if (fileElement.files && fileElement.files.length) {
+      const file = fileElement.files[0];
+      const filePlus: FilePlus = {
+        ...file, 
+        blobURL: URL.createObjectURL(file), 
+        cloudURL: "",
+        title: ""
+      };
+      this.files.unshift(filePlus);
+    }
+  }
 }
