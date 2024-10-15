256ce0ec1ecebca5b12e8e26d94d29b16fb7230f
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-10-15 16:16:47 +0100

    feat: Include new course card

--- src/app/exam/exam.component.ts
+++ src/app/exam/exam.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Exam, EXAMS } from '../models/exam';
+import { NEVER, Observable } from 'rxjs';
+import { ExamService } from '../services/exam.service';
 
 @Component({
   selector: 'app-exam',
   templateUrl: './exam.component.html',
   styleUrls: ['./exam.component.css']
 })
-export class ExamComponent {
+export class ExamComponent implements OnInit {
+  exam$: Observable<Exam> = NEVER;
 
+  constructor(private examService: ExamService) {}
+
+  ngOnInit(): void {
+    this.exam$ = this.examService.getItem$(EXAMS);
+  }
 }
