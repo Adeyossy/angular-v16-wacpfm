48080cefbb12402218576ba193756efcebbb29fe
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-10-15 20:34:31 +0100

    feat: Configure card grid item template

--- src/app/widgets/card-grid-item/card-grid-item.component.ts
+++ src/app/widgets/card-grid-item/card-grid-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-grid-item',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-grid-item.component.css']
 })
 export class CardGridItemComponent {
-
+  @Input() link = window.location.href;
+  @Input() title = "";
+  @Input() description = "";
+  @Input() linkText = "GO"
 }
