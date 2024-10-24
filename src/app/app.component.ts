cddd2b49f215a28e1f6966268148da25f5f2a7ef
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-10-24 09:51:57 +0100

    feat: Show navigation animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HelperService } from './services/helper.service';
+import { map, Observable, of } from 'rxjs';
+import { EventType, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(public helper: HelperService) {}
+export class AppComponent implements OnInit {
+  isNavigating$: Observable<boolean> = of(false);
+
+  constructor(public helper: HelperService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.isNavigating$ = this.router.events.pipe(
+      map(event => {
+        if (event.type === EventType.NavigationEnd || event.type === EventType.NavigationError ||
+          event.type === EventType.NavigationCancel || event.type === EventType.Scroll) {
+            console.log("navigation ended");
+            return false;
+        }
+        return true;
+      })
+    );
+  }
 }
