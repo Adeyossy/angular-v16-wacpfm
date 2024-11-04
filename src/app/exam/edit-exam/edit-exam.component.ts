868ac9baae47edf691f12cbc8c2241734ddd7232
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-11-04 14:11:09 +0100

    feat: Supply initial testing exam object

--- src/app/exam/edit-exam/edit-exam.component.ts
+++ src/app/exam/edit-exam/edit-exam.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { catchError, concatMap, map, Observable, of } from 'rxjs';
-import { Exam, EXAMS } from 'src/app/models/exam';
+import { Exam, EXAMS, NEW_EXAM } from 'src/app/models/exam';
 import { AuthService } from 'src/app/services/auth.service';
 import { HelperService } from 'src/app/services/helper.service';
 
@@ -9,11 +9,16 @@ import { HelperService } from 'src/app/services/helper.service';
   templateUrl: './edit-exam.component.html',
   styleUrls: ['./edit-exam.component.css']
 })
-export class EditExamComponent {
+export class EditExamComponent implements OnInit {
   @Input() exam$: Observable<Exam> = new Observable();
   updateTracker$: Observable<boolean> | null = null;
 
   constructor(private authService: AuthService, public helper: HelperService) {}
+
+  ngOnInit(): void {
+    // For testing purposes only
+    this.exam$ = of(NEW_EXAM);
+  }
 
   update$() {
     this.updateTracker$ = this.exam$.pipe(
