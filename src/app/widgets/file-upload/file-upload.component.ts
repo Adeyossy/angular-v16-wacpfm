e73b5da100b02e07f4e1f7643571e0846cacaf9c
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-11-13 21:22:24 +0100

    feat: Create, read, update and delete file(s)

--- src/app/widgets/file-upload/file-upload.component.ts
+++ src/app/widgets/file-upload/file-upload.component.ts
@@ -1,11 +1,10 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { map, NEVER, Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
 interface FilePlus extends File {
   blobURL: string;
   cloudURL: string;
-  title: string;
 }
 
 @Component({
@@ -13,28 +12,55 @@ interface FilePlus extends File {
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements OnInit {
   @Input() formats = "";
   @Input() path = "";
   @ViewChild('file') uploadFile: ElementRef = new ElementRef('input');
   @Input() files: FilePlus[] = [];
   @Output() fileEmitter = new EventEmitter<FilePlus>();
+  @Output() createEmitter = new EventEmitter<FilePlus>();
+  @Output() deleteEmitter = new EventEmitter<FilePlus>();
   @Output() linkEmitter = new EventEmitter<string>();
   uploadState$: Observable<number> = NEVER;
+  deleteState$: Observable<boolean> | null = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
-  onFileChosen() {
+  ngOnInit(): void {
+    if (this.files.length === 0) {
+      this.files = [ this.createNewFile() ];
+    }
+  }
+
+  createNewFile() {
+    const file = new File([], "");
+     return {
+        ...file,
+        blobURL: "",
+        cloudURL: ""
+      }
+  }
+
+  deleteFromApp(filePlus: FilePlus) {
+    this.deleteEmitter.emit(filePlus);
+  }
+
+  deleteFromCloud$(filePlus: FilePlus) {
+    this.authService.deleteFile$(filePlus.cloudURL).pipe(
+      map(_v => { this.deleteFromApp(filePlus) })
+    )
+  }
+
+  onFileChosen(filePlus: FilePlus) {
     const fileElement = this.uploadFile.nativeElement as HTMLInputElement;
     if (fileElement.files && fileElement.files.length) {
       const file = fileElement.files[0];
       const filePlus: FilePlus = {
         ...file, 
         blobURL: URL.createObjectURL(file), 
-        cloudURL: "",
-        title: ""
+        cloudURL: ""
       };
-      this.fileEmitter.emit(filePlus);
+      this.createEmitter.emit(filePlus);
     }
   }
 
