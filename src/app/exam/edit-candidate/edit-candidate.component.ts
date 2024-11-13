555c41ba58edd98df802f94f6b16510c85240a99
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-11-13 06:28:50 +0100

    fix: Implement corrections from meeting

--- src/app/exam/edit-candidate/edit-candidate.component.ts
+++ src/app/exam/edit-candidate/edit-candidate.component.ts
@@ -12,7 +12,7 @@ export class EditCandidateComponent implements OnInit {
   candidate$: Observable<Candidate> = new Observable();
 
   handicap = ["None", "Use of a wheelchair", "Use of walking frame or crutches", `Visual acuity 
-    worse than 3/60 despite correction`, `Severe hearing impairment despite hearing aid`, 
+    worse than 3/60 despite correction`, `Severe hearing impairment`, `Stammering`, 
     "Others"];
 
   updateTracker$: Observable<boolean> | null = null;
