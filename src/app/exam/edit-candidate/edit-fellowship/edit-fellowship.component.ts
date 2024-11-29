b0ece4c795581c0d1937fbdfb72bde5c55721f83
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-11-29 13:37:42 +0100

    feat: Upload fellowship dissertation and casebooks

--- src/app/exam/edit-candidate/edit-fellowship/edit-fellowship.component.ts
+++ src/app/exam/edit-candidate/edit-fellowship/edit-fellowship.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AcademicWriting, Dissertation } from 'src/app/models/candidate';
 
 @Component({
   selector: 'app-edit-fellowship',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-fellowship.component.css']
 })
 export class EditFellowshipComponent {
+  @Input() dissertations: Dissertation[] = [];
+  @Input() casebooks: AcademicWriting[] = [];
 
+  toCardList = (upload: AcademicWriting, index: number) => {
+    return { title: upload.title, subtitle: upload.type, text: `${index}` }
+  }
 }
