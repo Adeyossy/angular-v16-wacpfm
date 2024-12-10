b47e77f692ef60a4ea70405ee598a9dd8eab95bb
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-12-10 04:39:12 +0100

    fix: Use writing object

--- src/app/widgets/title-subtitle-file/title-subtitle-file.component.ts
+++ src/app/widgets/title-subtitle-file/title-subtitle-file.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Upload } from 'src/app/models/candidate';
+import { DEFAULT_UPLOAD, DEFAULT_WRITING, Upload, Writing, WritingType } from 'src/app/models/candidate';
 import { FilePlus } from '../file-upload/file-upload.component';
+import { serverTimestamp } from 'firebase/firestore';
 
 @Component({
   selector: 'app-title-subtitle-file',
@@ -8,19 +9,32 @@ import { FilePlus } from '../file-upload/file-upload.component';
   styleUrls: ['./title-subtitle-file.component.css']
 })
 export class TitleSubtitleFileComponent {
-  @Input() title = "";
-  @Input() subtitle? = "";
-  @Input() files: Upload[] = [];
+  @Input() writing: Writing = Object.assign({}, DEFAULT_WRITING)
   @Output() titleEmitter = new EventEmitter<string>();
   @Output() subtitleEmitter = new EventEmitter<string>();
 
   toFile = (file: Upload) => {
-    const newFile = new File([], "empty");
-    const filePlus: FilePlus = {
-      blobURL: "",
-      cloudURL: file.url,
-      ...newFile
-    }
-    return filePlus;
+    const newFile = new File([], "empty") as FilePlus;
+    newFile.blobURL = "";
+    newFile.cloudURL = file.url;
+    return newFile;
+  }
+
+  /**
+   * Converts files in FilePlus format for storage as Upload object
+   * @param filesPlus files from file-upload component
+   * @returns void
+   */
+  filePlusToUpload(filesPlus: FilePlus[]) {
+    return filesPlus.map(filePlus => {
+      const upload: Upload = {
+        description: "",
+        filetype: filePlus.type,
+        id: filePlus.lastModified,
+        type: this.writing.type,
+        uploadDate: serverTimestamp(),
+        url: filePlus.cloudURL
+      }
+    })
   }
 }
