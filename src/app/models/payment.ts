772b18fd80e79756064c1bab7793a79e16bb62de
Adeyosola Mustapha <adeyossy1@gmail.com>
2024-12-18 08:23:44 +0100

    feat: Update paystack webhook response model

--- src/app/models/payment.ts
+++ src/app/models/payment.ts
@@ -1,3 +1,5 @@
+import { UpdateCourseType } from "./update_course_record";
+
 export const PAYMENTS = "payments";
 
 export type Payment = {}
@@ -9,6 +11,41 @@ export interface PaystackInitResponse {
     authorization_url: string,
     access_code: string,
     reference: string
+  }
+}
+
+export interface PaystackResponse {
+  status: boolean,
+  message: string,
+  data: {
+    amount: number,
+    authorization_url: string,
+    customer: {
+      firstname: "",
+      lastname: "",
+      email: ""
+    },
+    metadata: {
+      custom_fields: [
+        {
+          display_name: "Category",
+          variable_name: "category",
+          value: UpdateCourseType
+        },
+        {
+          display_name: "Fee",
+          variable_name: "fee",
+          value: number
+        },
+        {
+          display_name: "Course ID",
+          variable_name: "course_id",
+          value: string
+        }
+      ]
+    },
+    reference: string,
+    status: string
   }
 }
 
@@ -24,7 +61,7 @@ export interface PaystackTransaction {
   reference: string;
   message: string;
   redirecturl: string;
-  status: "success";
+  status: string;
   trans: string;
   transaction: string;
   trxref: string;
