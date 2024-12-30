23cc0074722f6e29258d9e9a5807244e6c5293c2
Adeyosola Mustapha <adeyossy1@gmail.com>
2024-12-30 08:57:43 +0100

    fix: Download certificate for past courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,6 +53,7 @@ const routes: Routes = [
       { path: "updatecourse/:updateCourseId/details/payment/:category", component: UpdateCoursePaymentComponent },
       { path: "updatecourse/:updateCourseId/details/edit", component: NewCourseComponent },
       { path: "updatecourse/:updateCourseId/resourcepersons", component: ResourcePersonsDashComponent },
+      { path: "updatecourse/previous/:updateCourseId/details/certificate/:recordId", component: CertificateComponent },
       { path: "updatecourse/:updateCourseId/details/certificate/:recordId", component: CertificateComponent },
       { path: "admin", component: AdminComponent, canActivate: [adminGuard] },
       { path: "exam", component: ExamComponent, title: "Examination | Faculty of Family Medicine App" },
