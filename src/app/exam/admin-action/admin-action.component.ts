3b057c786e5cc696d698d270b262aafccba0ecd4
Adeyosola Mustapha <adeyossy1@gmail.com>
2025-02-27 04:24:43 +0100

    feat: Fetch exam and examiner details

--- src/app/exam/admin-action/admin-action.component.ts
+++ src/app/exam/admin-action/admin-action.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { concatMap, map, Observable, of } from 'rxjs';
+import { Exam } from 'src/app/models/exam';
+import { Examiner } from 'src/app/models/examiner';
+import { ExamService } from 'src/app/services/exam.service';
 
 @Component({
   selector: 'app-admin-action',
   templateUrl: './admin-action.component.html',
   styleUrls: ['./admin-action.component.css']
 })
-export class AdminActionComponent {
+export class AdminActionComponent implements OnInit {
+  exam$: Observable<Exam[]> = of([]);
+  examiners$: Observable<Examiner[]> = of([]);
 
+  constructor(private examService: ExamService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const examAlias$ = this.activatedRoute.paramMap.pipe(
+      map(params => params.get("examAlias")),
+      map(examAlias => examAlias === null ? "" : examAlias)
+    );
+
+    this.exam$ = examAlias$.pipe(
+      concatMap(examAlias => this.examService.queryExam$(examAlias))
+    );
+
+    this.examiners$ = examAlias$.pipe(
+      concatMap(examAlias => this.examService.queryExaminers$(examAlias))
+    );
+  }
 }
