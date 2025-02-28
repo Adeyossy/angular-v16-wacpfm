f371e897a1d918c005b92e4cff871762ef97c0fe
Adeyosola Mustapha <adeyossy1@gmail.com>
2025-02-28 08:54:44 +0100

    feat: Show list of examiners

--- src/app/exam/admin-action/admin-action.component.ts
+++ src/app/exam/admin-action/admin-action.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { concatMap, map, Observable, of } from 'rxjs';
+import { Candidate } from 'src/app/models/candidate';
 import { Exam } from 'src/app/models/exam';
 import { Examiner } from 'src/app/models/examiner';
 import { ExamService } from 'src/app/services/exam.service';
+import { CardList } from 'src/app/widgets/card-list/card-list.component';
 
 @Component({
   selector: 'app-admin-action',
@@ -13,6 +15,9 @@ import { ExamService } from 'src/app/services/exam.service';
 export class AdminActionComponent implements OnInit {
   exam$: Observable<Exam[]> = of([]);
   examiners$: Observable<Examiner[]> = of([]);
+  examinersList$: Observable<CardList[]> = of([]);
+  candidates$: Observable<Candidate[]> = of([]);
+  candidatesList$: Observable<CardList[]> = of([]);
 
   constructor(private examService: ExamService, private activatedRoute: ActivatedRoute) {}
 
@@ -29,5 +34,18 @@ export class AdminActionComponent implements OnInit {
     this.examiners$ = examAlias$.pipe(
       concatMap(examAlias => this.examService.queryExaminers$(examAlias))
     );
+
+    this.examinersList$ = this.examiners$.pipe(
+      concatMap(examiners => {
+        const examinersList = examiners.map(examiner => {
+          return {
+            title: examiner.name,
+            subtitle: examiner.country,
+            text: examiner.userEmail
+          }
+        });
+        return of(examinersList);
+      })
+    )
   }
 }
