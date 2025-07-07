ad35793c17dd1cc3148b3347e0c7fe83b3ee2ca2
Adeyosola Mustapha <adeyossy1@gmail.com>
2025-07-08 00:47:09 +0100

    feat: Create endpoint for fetching paystack config

---
+++ netlify/functions/paystack-config.ts
@@ -0,0 +1,17 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const paystackConfig = {
+  test_pk: process.env['PAYSTACK_TEST_PK'],
+  live_pk: process.env['PAYSTACK_LIVE_PK'],
+  test_sk: process.env['PAYSTACK_TEST_SK'],
+  live_sk: process.env['PAYSTACK_LIVE_SK']
+};
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Object.assign(paystackConfig))
+  }
+}
+
+export { handler };
