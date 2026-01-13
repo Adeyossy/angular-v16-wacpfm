e476f5a64badf912a6c0f73a347cf1efc25ac796
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-13 13:33:28 +0100

    feat: Diplay list of portfolio sections

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardList } from '../widgets/card-list/card-list.component';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-
+  sections: CardList[] = [
+    {
+      title: "Section 3",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 4",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 5",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 6",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 7",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 8",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 9",
+      subtitle: "",
+      text: ""
+    },
+    {
+      title: "Section 10",
+      subtitle: "",
+      text: ""
+    }
+  ]
 }
