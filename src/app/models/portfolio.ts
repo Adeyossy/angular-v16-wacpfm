9d28a8d50decdd1e71f28cf12354bc00cdd969ad
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-14 19:27:02 +0100

    feat: Define default portfolio section item

--- src/app/models/portfolio.ts
+++ src/app/models/portfolio.ts
@@ -1,5 +1,6 @@
-import { FieldValue } from "firebase/firestore";
+import { FieldValue, serverTimestamp } from "firebase/firestore";
 import { environment } from "src/environments/environment";
+import { Upload } from "./candidate";
 
 export const PORTFOLIO_COLLECTION = environment.portfolio;
 
@@ -7,14 +8,30 @@ export interface PortfolioSectionItem {
   id: string,
   userId: string,
   email: string,
-  category: "Membership" | "Fellowship",
+  category: "Membership" | "Fellowship" | "",
   title: string,
   description: string,
-  url: string,
+  files: Upload[],
   grade: string,
   section: string,
   subsection: string,
   submission_timestamp: FieldValue,
   examinerId: string,
   examinerEmail: string
+}
+
+export const DEFAULT_PORTFOLIO_SECTION_ITEM: PortfolioSectionItem = {
+  id: "",
+  userId: "",
+  email: "",
+  category: "",
+  title: "",
+  description: "",
+  files: [],
+  grade: "",
+  section: "",
+  subsection: "",
+  submission_timestamp: serverTimestamp(),
+  examinerId: "",
+  examinerEmail: ""
 }
