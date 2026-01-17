a0d5e0d4a9452d431a29b9774d569d14f5dc57c1
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-17 08:34:09 +0100

    feat: Base list on portfolio section model

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,52 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardList } from '../widgets/card-list/card-list.component';
+import { PortfolioService } from '../services/portfolio.service';
+import { PortfolioSection, SECTIONS } from '../models/portfolio-section';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
   sections: CardList[] = [
     {
       title: "Section 3",
-      subtitle: "",
+      subtitle: "Learning Plans and Rotations",
       text: ""
     },
     {
       title: "Section 4",
-      subtitle: "",
+      subtitle: "Record of Education Supervision",
       text: ""
     },
     {
       title: "Section 5",
-      subtitle: "",
+      subtitle: "Observations by Supervisors",
       text: ""
     },
     {
       title: "Section 6",
-      subtitle: "",
-      text: ""
-    },
-    {
-      title: "Section 7",
-      subtitle: "",
+      subtitle: "Written Assignments",
       text: ""
     },
     {
       title: "Section 8",
-      subtitle: "",
+      subtitle: "Emergency Medicine Certification",
       text: ""
     },
     {
       title: "Section 9",
-      subtitle: "",
+      subtitle: "Professional and Scientific Meetings",
       text: ""
     },
     {
       title: "Section 10",
-      subtitle: "",
+      subtitle: "End of Year Assessments",
       text: ""
     }
-  ]
+  ];
+
+  constructor (private portfolioService: PortfolioService) {}
+
+  ngOnInit(): void {
+    this.sections = SECTIONS.map(this.toCardList)
+  }
+
+  toCardList = (section: PortfolioSection): CardList => {
+    return {
+      title: section.section,
+      subtitle: section.description,
+      text: "-"
+    }
+  }
 }
