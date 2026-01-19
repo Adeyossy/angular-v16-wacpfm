deecfd6eaf11abc4fcc5c332d855c148672daa9b
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-19 11:17:37 +0100

    feat: Show list of candidates in admin

--- src/app/exam/admin-candidates/admin-candidates.component.ts
+++ src/app/exam/admin-candidates/admin-candidates.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { concatMap, Observable, of } from 'rxjs';
+import { FellowshipExamRecord, MembershipExamRecord } from 'src/app/models/candidate';
+import { ExamService } from 'src/app/services/exam.service';
+import { CardList } from 'src/app/widgets/card-list/card-list.component';
 
 @Component({
   selector: 'app-admin-candidates',
   templateUrl: './admin-candidates.component.html',
   styleUrls: ['./admin-candidates.component.css']
 })
-export class AdminCandidatesComponent {
+export class AdminCandidatesComponent implements OnInit {
+  // This should lead to another route/screen/component with Membership or Fellowship dichotomies
+  // For now this list will have all candidates together.
 
+  candidates$: Observable<(MembershipExamRecord | FellowshipExamRecord)[]> = of([]);
+
+  constructor (
+    private examService: ExamService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.candidates$ = this.examService.parseExamAliasFromRoute$(this.activatedRoute.paramMap)
+    .pipe(
+      concatMap(this.examService.queryCandidates$)
+    );
+  }
+
+  toCardList = (candidate: MembershipExamRecord | FellowshipExamRecord): CardList => {
+    return {
+      title: candidate.wacpNo,
+      subtitle: candidate.examNo,
+      text: candidate.category
+    }
+  }
 }
