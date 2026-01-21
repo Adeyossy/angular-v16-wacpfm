24bc3bfa0d5f757f36c3e917e1241dd69d3aa9aa
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-21 06:59:31 +0100

    feat: Group portfolio items by subsection

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -5,6 +5,7 @@ import { PortfolioService } from '../services/portfolio.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PortfolioSectionItem } from '../models/portfolio';
 import { FieldValue, serverTimestamp, Timestamp } from 'firebase/firestore';
+import { USERID_ROUTE_PARAM } from '../app-routing.module';
 
 @Component({
   selector: 'app-portfolio-section',
@@ -12,10 +13,13 @@ import { FieldValue, serverTimestamp, Timestamp } from 'firebase/firestore';
   styleUrls: ['./portfolio-section.component.css']
 })
 export class PortfolioSectionComponent implements OnInit {
+  userId$ = of("");
+  userIdParam$ = of("");
   section = of("");
   sectionItems$: Observable<PortfolioSectionItem[]> = of([]);
   sectionItemsList: { [index: string]: CardList } = {};
   sectionItemsList$: Observable<CardList[]> = of([]);
+  sectionItemsGroupsList$: Observable<{subsection: string, items: PortfolioSectionItem[]}[]> = of([]);
   newItem$: Observable<string> | null = null;
 
   constructor (
@@ -29,16 +33,43 @@ export class PortfolioSectionComponent implements OnInit {
       map(paramMap => paramMap.get("section") ? paramMap.get("section")! : "")
     );
 
+    this.userId$ = this.portfolioService.getUser$().pipe(
+      map(user => user.uid)
+    );
+    
+    this.userIdParam$ = this.portfolioService.parseParamFromRoute$(
+      this.activatedRoute.paramMap, USERID_ROUTE_PARAM
+    );
+
     this.sectionItems$ = this.section.pipe(
-      concatMap(this.portfolioService.getUserPortfolioSection$)
+      concatMap(section => this.userId$.pipe(
+        concatMap(userId => this.portfolioService.getPortfolioSection$(section, userId))
+      ))
     );
 
     this.sectionItemsList$ = this.sectionItems$.pipe(
       map(items => items.map(this.toCardList))
     );
+
+    this.sectionItemsGroupsList$ = this.section.pipe(
+      concatMap(section => this.userId$.pipe(
+        concatMap(userId => this.portfolioService.getPortfolioSection$(section, userId)),
+        map(items => this.groupBySubsections(items, section).sort((a, b) => b.items.length - a.items.length))
+      ))
+    );
   }
 
   toCardList = (sectionItem: PortfolioSectionItem): CardList => {
+    const timestamp = sectionItem.submission_timestamp as unknown as Timestamp;
+    
+    return {
+      title: sectionItem.subsection,
+      subtitle: timestamp.toDate().toLocaleString("en-NG"),
+      text: sectionItem.category
+    };
+  }
+
+  cachedToCardList = (sectionItem: PortfolioSectionItem): CardList => {
     const existing = this.sectionItemsList[sectionItem.id];
     if (existing) return existing;
 
@@ -53,6 +84,17 @@ export class PortfolioSectionComponent implements OnInit {
     return cardList;
   }
 
+  groupBySubsections = (sectionItems: PortfolioSectionItem[], sectionId: string) => {
+    const subsections = this.portfolioService.getSubsections(sectionId);
+    return subsections.map(subsection => ({
+      subsection: subsection.subsection,
+      items: sectionItems.filter(
+        sectionItem => sectionItem.section === sectionId && 
+        sectionItem.subsection === subsection.subsection
+      )
+    }));
+  }
+
   addNewItem$ = () => {
     this.newItem$ = this.portfolioService.createPortfolioSectionItemRef$().pipe(
       map(id => {
@@ -60,5 +102,9 @@ export class PortfolioSectionComponent implements OnInit {
         return id;
       })
     );
+  }
+
+  navigate = (shouldNavigate: boolean, url: string) => {
+    if (shouldNavigate) this.router.navigate([this.router.url, url]);
   }
 }
