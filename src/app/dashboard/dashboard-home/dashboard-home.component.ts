973e1af94ac09240db9ab400ba6ba1d3a9250d90
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-01-21 06:54:28 +0100

    feat: Append userId to end of route

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map, Observable, of } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { CardGrid } from 'src/app/widgets/card-grid/card-grid.component';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.css']
 })
-export class DashboardHomeComponent {
+export class DashboardHomeComponent implements OnInit {
+  userId$ = of("");
   items = [
     {
       title: "Update Course",
@@ -27,5 +31,25 @@ export class DashboardHomeComponent {
       description: "",
       link: "/dashboard/portfolio"
     }
-  ]
+  ];
+  items$: Observable<CardGrid[]> = of([]);
+
+  constructor (private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userId$ = this.authService.getFirebaseUser$().pipe(
+      map(user => user.uid)
+    );
+
+    this.items$ = this.userId$.pipe(
+      map(this.modifyItems)
+    )
+  }
+
+  modifyItems = (userId: string) => {
+    return this.items.map(i => {
+      if (i.title === "Portfolio") i.link = i.link.concat(`/${userId}`);
+      return i;
+    });
+  }
 }
