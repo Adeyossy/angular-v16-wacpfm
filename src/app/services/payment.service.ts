e3ede8ee0dc2849557990f0e6ce63345dbb992ee
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-06-06 18:41:02 +0100

    feat: Initiate payment functions transfer

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import PaystackPop from '@paystack/inline-js';
 import { EventService } from './event.service';
 import { UpdateCourseService } from './update-course.service';
+import { TransactionParamsWithSK, TransactionResponse } from '../models/payment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 type Channels = ["card", "bank_transfer", "apple_pay", "ussd", "qr", "mobile_money", "eft"];
 
@@ -13,10 +16,21 @@ export class PaymentService {
   channels: Channels = [
     "card", "bank_transfer", "bank", "apple_pay", "ussd", "qr", "mobile_money", "eft"
   ] as unknown as Channels;
+  backendUrl = "/.netlify/functions";
 
-  constructor(private eventService: EventService, private courseService: UpdateCourseService) { }
+  constructor(
+    private eventService: EventService, 
+    private courseService: UpdateCourseService,
+    private httpClient: HttpClient
+  ) { }
 
-  getPaymentList (type: string, id: string) {
+  getTransaction(options: TransactionParamsWithSK) {
+    return this.httpClient.post<string>(
+      `${this.backendUrl}/get-transaction`, options
+    );
+  }
+
+  getPaymentList(type: string, id: string) {
     if (type === "updatecourse") {
       return this.courseService.getPaymentsList$(id);
     }
@@ -26,5 +40,24 @@ export class PaymentService {
 
   getEventPayments() {
     return this.eventService.getAllPayments$("2025-07-07");
+  }
+
+  getAllPayments$ = (date: string) => {
+    return this.getTransaction({
+      secret_key: "PAYSTACK_LIVE_SK",
+      params: {
+        from: date,
+        status: "success",
+        perPage: 500
+      }
+    }).pipe(
+      map(res => {
+        const response = JSON.parse(res) as unknown as TransactionResponse;
+        if (response.data && response.data.length) {
+          return response.data;
+        }
+        return [];
+      })
+    )
   }
 }
