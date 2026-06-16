cc2077303c472838ed81127a3804e94789375a68
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-06-16 13:59:22 +0100

    feat: Base link on platform property

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../services/event.service';
+import { concatMap, Observable, of } from 'rxjs';
+import { DEFAULT_NEW_EVENT, Event } from '../models/event';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
-export class EventComponent {
+export class EventComponent implements OnInit {
+  event$: Observable<Event> = of(DEFAULT_NEW_EVENT);
+  
+  constructor (private eventService: EventService, private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.event$ = this.eventService.getEventId$(this.activatedRoute.paramMap).pipe(
+      concatMap(this.eventService.getEventById$)
+    );
+  }
 }
