120311bac76302ee25bbf43db7d26ab141819553
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-06-16 14:19:26 +0100

    feat: Refine transaction responses

--- src/app/models/payment.ts
+++ src/app/models/payment.ts
@@ -150,84 +150,90 @@ export interface VerifyTransactionResponse extends BasicResponse {
   }
 }
 
-export interface UpdateCourseTransaction extends BasicResponse {
-  event: string,
-  data: {
+export interface UpdateCourseTransactionData {
+  id: number,
+  domain: string,
+  status: string,
+  reference: string,
+  amount: number,
+  message: null,
+  gateway_response: string,
+  paid_at: string,
+  created_at: string,
+  channel: string,
+  currency: string,
+  ip_address: string,
+  metadata: {
+    custom_fields: [
+      {
+        display_name: "Category",
+        variable_name: "category",
+        value: PaymentCategory
+      },
+      {
+        display_name: "Fee",
+        variable_name: "fee",
+        value: number
+      },
+      {
+        display_name: "Course ID",
+        variable_name: "course_id",
+        value: string
+      }
+    ]
+  },
+  log: {
+    time_spent: number,
+    attempts: number,
+    authentication: string,
+    errors: number,
+    success: false,
+    mobile: false,
+    input: [],
+    channel: null,
+    history: Array<
+      {
+        type: string,
+        message: string,
+        time: number
+      }
+    >
+  },
+  fees: null,
+  customer: {
     id: number,
-    domain: string,
-    status: string,
-    reference: string,
-    amount: number,
-    message: null,
-    gateway_response: string,
-    paid_at: string,
-    created_at: string,
-    channel: string,
-    currency: string,
-    ip_address: string,
-    metadata: {
-      custom_fields: [
-        {
-          display_name: "Category",
-          variable_name: "category",
-          value: PaymentCategory
-        },
-        {
-          display_name: "Fee",
-          variable_name: "fee",
-          value: number
-        },
-        {
-          display_name: "Course ID",
-          variable_name: "course_id",
-          value: string
-        }
-      ]
-    },
-    log: {
-      time_spent: number,
-      attempts: number,
-      authentication: string,
-      errors: number,
-      success: false,
-      mobile: false,
-      input: [],
-      channel: null,
-      history: Array<
-        {
-          type: string,
-          message: string,
-          time: number
-        }
-      >
-    },
-    fees: null,
-    customer: {
-      id: number,
-      first_name: string,
-      last_name: string,
-      email: string,
-      customer_code: string,
-      phone: null,
-      metadata: null,
-      risk_action: string
-    },
-    authorization: {
-      authorization_code: string,
-      bin: string,
-      last4: string,
-      exp_month: string,
-      exp_year: string,
-      card_type: string,
-      bank: string,
-      country_code: string,
-      brand: string,
-      account_name: string
-    },
-    plan: {}
-  }
+    first_name: string,
+    last_name: string,
+    email: string,
+    customer_code: string,
+    phone: null,
+    metadata: null,
+    risk_action: string
+  },
+  authorization: {
+    authorization_code: string,
+    bin: string,
+    last4: string,
+    exp_month: string,
+    exp_year: string,
+    card_type: string,
+    bank: string,
+    country_code: string,
+    brand: string,
+    account_name: string
+  },
+  plan: {}
 }
 
+export interface UpdateCourseTransaction extends BasicResponse {
+  event: string,
+  data: UpdateCourseTransactionData
+}
+
+export interface UpdateCourseTransactions {
+  event: string,
+  data: UpdateCourseTransactionData[]
+}
 
 export interface TransactionResponse {
   status: boolean,
