01ad358e952cb4ef96b4727505118f07d8c90d88
Adeyosola Mustapha <adeyossy1@gmail.com>
2026-06-26 14:38:57 +0100

    feat: Configure component

--- src/app/widgets/card-list-item/card-list-item.component.ts
+++ src/app/widgets/card-list-item/card-list-item.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface CardListItem {
+  title: string;
+  subtitle: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-card-list-item',
@@ -6,5 +12,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-list-item.component.css']
 })
 export class CardListItemComponent {
-
+  @Input() item: CardListItem = { title: "", subtitle: "", text: "" }
+  @Input() index: number = -1;
+  @Input() control = false;
+  @Input() state = false;
 }
